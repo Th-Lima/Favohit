f8412be86954740ac6471e3903ac1f74b80884ea
Thales Lima <M6104902@mgmt.tlrg.com>
2021-01-28 19:40:10 -0300

    First commit

---
+++ Favohit.WebApi/Favohit.WebApi/Controllers/AlbumController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Favohit.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class AlbumController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new 
+            { 
+                Id = Guid.NewGuid().ToString(), 
+                Title = "Album 0001",
+                Description = "Lorem ipsum"
+            });
+        }
+    }
+}
