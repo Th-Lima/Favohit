3d2f52febc4db04bfcc3be3a79a03169d7653102
Thales Lima <lthales53@gmail.com>
2021-01-29 20:08:51 -0300

    Implementing repository pattern

---
+++ Favohit.WebApi/Repository/Interface/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Favohit.WebApi.Repository.Interface
+{
+    interface IRepository<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetById(Guid id);
+
+        Task<T> Save(T obj);
+
+        Task<T> Remove(T obj);
+
+        Task<T> Update(T obj);
+    }
+}
---
+++ Favohit.WebApi/Repository/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using Favohit.WebApi.Models;
+using Favohit.WebApi.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Favohit.WebApi.Repository
+{
+    public class UserRepository : IRepository<User>
+    {
+        public Task<IEnumerable<User>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Remove(User obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Save(User obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> Update(User obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
