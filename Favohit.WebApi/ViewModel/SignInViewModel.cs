94fcd6e4f5e08ad1762686520bc6c16a95cbf7d6
Thales Lima <lthales53@gmail.com>
2021-02-02 19:12:41 -0300

    Implementing ViewModels

---
+++ Favohit.WebApi/ViewModel/SignInViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Favohit.WebApi.ViewModel
+{
+    public class SignInViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
