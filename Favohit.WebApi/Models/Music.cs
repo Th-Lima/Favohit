9118c6574f210f868bc2a97a61c61f436538b2a2
Thales Lima <lthales53@gmail.com>
2021-02-02 17:19:19 -0300

    Including DataAnnotations in models

--- Favohit.WebApi/Models/Music.cs
+++ Favohit.WebApi/Models/Music.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -9,9 +10,11 @@ namespace Favohit.WebApi.Models
     public class Music
     {
         public Guid Id { get; set; }
-        
+
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public int Duration { get; set; }
 
         [JsonIgnore]
