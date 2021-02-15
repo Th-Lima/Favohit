8e060c8f63fd87001bc6b43287db7cc45844e486
Thales Lima <lthales53@gmail.com>
2021-02-15 18:07:39 -0300

    Implementing music detail and adding music list in album model

--- Favohit.Ui/src/app/main/pages/music/music-detail/music-detail.component.ts
+++ Favohit.Ui/src/app/main/pages/music/music-detail/music-detail.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { forkJoin } from "rxjs";
-import Swal from "sweetalert2";
+import { MusicService } from './../../../../services/music.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import Album from 'app/model/album.model';
+import { forkJoin } from 'rxjs';
+import Swal from 'sweetalert2';
+import Music from 'app/model/music.model.ts';
 
 @Component({
-    selector: "app-music-detail",
-    templateUrl: "./music-detail.component.html",
-    styleUrls: ["./music-detail.component.scss"],
+    selector: 'app-music-detail',
+    templateUrl: './music-detail.component.html',
+    styleUrls: ['./music-detail.component.scss'],
 })
 export class MusicDetailComponent implements OnInit {
-    constructor() {}
+    
+    private albumId: string;
+    album: Album;
 
-    ngOnInit() {}
+    constructor(private service: MusicService, private router: Router, private route: ActivatedRoute) {}
+
+    ngOnInit() {
+        this.albumId = this.route.snapshot.paramMap.get('id');
+
+        this.service.getAlbumDetail(this.albumId).subscribe(data => {
+            this.album = data;
+        });
+    }
+
+    getMusicDuration(value: number): string {
+        const minutes: number = Math.floor(value / 60);
+        return `${minutes.toString().padStart(2, "0")}: ${(value - minutes * 60).toString().padStart(2, "0")}`;
+    }
 }
--- Favohit.Ui/src/app/model/album.model.ts
+++ Favohit.Ui/src/app/model/album.model.ts
@@ -1,7 +1,10 @@
+import Music from "./music.model.ts";
+
 export default class Album {
     public id?: string;
     public name?: string;
     public description?: string;
     public backdrop?: string;
     public band?: string;
+    public musics?: Music[];
 }
