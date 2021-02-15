66f50430308aa48a63ecc8d82b74c0ceff1c9374
Thales Lima <lthales53@gmail.com>
2021-02-15 19:51:45 -0300

    Implement login component

--- Favohit.Ui/src/app/main/pages/authentication/login/login.component.ts
+++ Favohit.Ui/src/app/main/pages/authentication/login/login.component.ts
@@ -1,3 +1,5 @@
+import { PersistenceService } from './../../../../services/persistence.service';
+import { UserService } from './../../../../services/user.service';
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
@@ -15,7 +17,10 @@ export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     constructor(
         private fuseConfigService: FuseConfigService,
-        private formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private service: UserService,
+        private router: Router,
+        private persistence: PersistenceService
     ) {
         this.fuseConfigService.config = {
             layout: {
@@ -40,5 +45,41 @@ export class LoginComponent implements OnInit {
             email: ["", [Validators.required, Validators.email]],
             password: ["", Validators.required],
         });
+    }
+
+    login(loginForm): void{
+        if (loginForm.isValid === false){
+            return;
+        }
+        let email = this.loginForm.get('email').value;
+        let password = this.loginForm.get('password').value;
+
+        this.service.authenticate({
+            email, 
+            password
+        }).subscribe((data) =>{
+
+            this.persistence.set('authenticate_user', data);
+            this.router.navigate(['/']);
+            this.showMenus();
+
+        }, (error) => {
+            if (error.status === 422){
+                swal.fire('Ops', 'Email ou senha invalido', 'error');
+            }
+        });
+    }
+
+    showMenus(): void{
+        this.fuseConfigService.config = {
+            layout: {
+                navbar:{
+                    hidden: false,
+                },
+                toolbar:{
+                    hidden: false,
+                },
+            },
+        };
     }
 }
